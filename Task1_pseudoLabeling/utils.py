39b329ef50e827a0b50f607496f40a852c001dba
kevinYitshak <kr070597@gmail.com>
2022-03-11 22:31:42 +0100

    task1 changed utils

--- Task1_pseudoLabeling/utils.py
+++ Task1_pseudoLabeling/utils.py
@@ -21,3 +21,22 @@ def accuracy(output, target, topk=(1,)):
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
 
+class AverageMeter(object):
+    """Computes and stores the average and current value
+       Imported from https://github.com/pytorch/examples/blob/master/imagenet/main.py#L247-L262
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
